9439b11b1dcc6e7b0a52338bde51fff571e6128e
Niqujoe <nikie.deocampo@gmail.com>
2022-01-07 17:15:05 +0800

    Another Edit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          John Francis Chua <code>src/App.js</code> and save to reload.
+          John Francis Patingo <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
